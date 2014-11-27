b67251d77f78a16468c76fdd91c0bfc6f54b578c
Emmanuel-A <e.amponsah@hotmail.ca>
2014-11-27 13:43:29 -0500

    Display countdown
    Style countdown timer and display it on the game screen

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -4,28 +4,43 @@ jQuery(function($) {
    
     // Create New Socket Connection using Socket.io
     var socket = io();
+    var userID;
+
+    // by default socket.io is a function... that takes two parameters
+    // redefine what socket.io represents
+    // socket.on = ('connected');
+    socket.on('updateUsersID', function(id){
+      userID = id;
+    });
 
     // Send A Message To The Server
     $('#rock').on('click', function(){
-      var text = $('input').val();
-      socket.emit('message', text);
-      alert('Rock!');
+      var obj = {id: userID, choice: 'rock'}; 
+      socket.emit('playerchoice', obj);
+      console.log('You chose rock');
     });
 
     $('#paper').on('click', function(){
-        var text = $('input').val();
-        socket.emit('message', text);
-        alert('Paper!');
-      });
-
-    $('#scissors').on('click', function(){
-      var text = $('input').val();
-      socket.emit('message', text);
-      alert('Scissors!');
+      var obj = {id: userID, choice: 'paper'}; 
+      socket.emit('playerchoice', obj);
+      console.log('You chose paper');
     });
 
-    // Recieve Update Event From The Server
-    socket.on('update', function(msg){
-      $('.game').append(msg).append('<br>');
+    $('#scissors').on('click', function(){
+      var obj = {id: userID, choice: 'scissors'}; 
+      socket.emit('playerchoice', obj);
+      console.log('You chose scissors');
+    });
+
+    socket.on('result', function(text){
+      console.log(text);
+      $('#result').html(text);
+    });
+
+    socket.on('countdown', function(text){
+      console.log(text);
+      for (var i = 1; i > 0; --i){
+        $('#countdown').html(text);
+      }  
     });
 });
--- server.js
+++ server.js
@@ -6,8 +6,12 @@ var http = require('http');
 http = http.Server(app);
 var io = require('socket.io');
 io = io(http);
-var users = [];
-var id = 0;
+var id = 1;
+var playerone;
+var playertwo;
+var playeronechoice;
+var playertwochoice;
+var countdown = 5;
 
 // Route our Assets
 app.use('/assets/', express.static(__dirname + '/public/assets/'));
@@ -20,27 +24,105 @@ app.get('/', function(req, res){
 // Handle Socket Connection 
 io.on('connection', function(socket){
 
-  console.log('A User Connected', id);
+  // Store user id on the socket itself
+  socket.id = id;
 
-  // Add user to users array
-  users.push({id: id, x:0, y:0});
+  console.log('A User Is Connected', id);
+
+  // Set playerone and playertwo to equal id
+  if(!playerone){
+    playerone = id;
+  } else if(!playertwo){
+    playertwo = id;
+  }
+
+  // Emit to the client their id
+  io.emit('updateUsersID', id);
 
   // Increment
   id = id + 1;
 
-  // Emit to the client their id
-  io.emit('connected', id);
-
-  // Handle Message Event
-  // socket.on('move', function(coord){
-  // 	console.log(coord);
-  //   io.emit('update', coord);
-  // });
-
-  socket.on('message', function(text){
-    io.emit('update', text);
+  // Listen to players choices
+  socket.on('playerchoice', function(obj){
+    if(obj.id == playerone){
+      playeronechoice = obj.choice;
+    } else if(obj.id == playertwo){
+      playeronechoice = obj.choice;
+    }
   });
 });
+
+io.on('disconnect', function(socket){
+  if(socket.id == playerone){
+    playerone = null;
+  } else if(socket.id == playertwo){
+    playertwo = null;
+  }
+});
+
+// var win = function() {
+//     document.getElementById("game").style.backgroundColor = "red";
+// };
+
+// Game logic
+  var gameLoop = function(){
+    var text = countdown;
+    io.emit('countdown', text);
+    countdown = countdown - 1;
+    if (countdown <= 0){
+      countdown = 5;
+      compare();
+    };
+    setTimeout(gameLoop, 1500);
+  };
+  gameLoop();
+
+  var getComputerChoice = function (){
+    var num = Math.floor(Math.random()*3);
+    var choices = ['rock','paper','scissors'];
+    return choices[num];
+  };
+
+  var compare = function(){
+    var a = playeronechoice;
+    var b = playertwochoice;
+    var text;
+
+    // Randomly generates choices if a or b are not set
+    if(!a){
+      a = getComputerChoice();
+    }
+    if(!b){
+      b = getComputerChoice();
+    }
+
+    if(a === b){
+      text = 'draw';      
+    }
+    else if(a === 'rock' && b=== 'scissors'){
+      text = 'player threw:' + a +' computer threw:'+ b + ' you win!';
+    }
+    else if(a === 'rock' && b === 'paper'){
+      text = 'player threw:' + a +' computer threw:'+ b + ' you lose!';
+    }
+    else if(a === 'paper' && b === 'scissors'){
+      text = 'player threw:' + a +' computer threw:'+ b + ' you lose!';
+    }
+    else if(a === 'paper' && b === 'rock'){
+      text = 'player threw:' + a +' computer threw:'+ b + ' you win!';
+      // win();
+    }
+    else if(a === 'scissors' && b === 'paper'){
+      text = 'player threw:' + a+' computer threw:'+ b + ' you win!';
+      // win();      
+    }
+    else if(a === 'scissors' && b === 'rock'){
+      text = 'player threw:' + a +' computer threw:'+ b + ' you lose!';
+    } 
+    io.emit('result', text);
+    playeronechoice = null;
+    playertwochoice = null;
+  };
 
 // Start Server
 http.listen(process.env.PORT || 3000, process.env.IP || "127.0.0.1", function(){
